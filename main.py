006d5be4b0ba907f0a792b5f1726d402342313b3
andrewzaia <andrew.zaia@outlook.com.au>
2024-09-16 22:44:05 +1000

    Note to change URL

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
 import requests
-import bs4
-
 
 def quote_of_the_day():
-    url = "https://www.brainyquote.com/quote_of_the_day"
+    url = "https://api.quotable.io/random" # Need to update URL
 
-    header = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    }
-
-    response = requests.get(url, headers=header)
+    response = requests.get(url)
 
     if response.ok:
-        site = bs4.BeautifulSoup(response.text, "html.parser")
-        quote_container = site.find("img", {"class": "p-qotd"})
-        if quote_container and quote_container.has_attr("alt"):
-            # Split quote
-            split_quote = quote_container["alt"].split(" - ")
-            return f"{split_quote[0]}\n- {split_quote[1]}"
-        else:
-            return "Quote not found."
+        data = response.json()
+        quote = data['content']
+        author = data['author']
+        return f"{quote}\n- {author}"
     else:
-        return f"Error accessing website. Response: {response}"
-
+        return f"Error accessing API. Response: {response.status_code}"
 
 get_quote = quote_of_the_day()
 print(get_quote)
